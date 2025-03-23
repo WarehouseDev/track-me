3aae76d1c1b7b23b66dd38c4a8fa12ccabd69ab4
Warehouse Dev <devfromthewarehouse@gmail.com>
2025-03-23 15:20:02 +0100

    Final commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const map = L.map("map");
+map.setView([0, 0], 15);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+marker.bindTooltip(
+  `<img src="./assets/logo.png" alt="logo" style="width:5rem;" /> <br/> is here!`
+);
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const userLat = position.coords.latitude;
+      const userLng = position.coords.longitude;
+
+      marker.setLatLng([userLat, userLng]);
+      map.setView([userLat, userLng], 15);
+    },
+    (error) => {
+      console.error(`Geolocation error: ${error.message}`);
+    },
+    {
+      enableHighAccuracy: true,
+      maximumAge: 0,
+      timeout: 5000,
+    }
+  );
+} else {
+  console.error("Geolocation is not supported by this browser.");
+}
