740cd614a39db856cd017d1b5a67f15f3f1e6b56
Warehouse Dev <devfromthewarehouse@gmail.com>
2025-03-24 14:57:02 +0100

    changes for safari and other browsers

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const map = L.map("map");
 map.setView([0, 0], 15);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "© OpenStreetMap contributors",
+// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   attribution: "© OpenStreetMap contributors",
+// }).addTo(map);
+
+// *******another tile provider*******
+L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+  attribution: "© OpenStreetMap & CartoDB contributors",
 }).addTo(map);
 
 const marker = L.marker([0, 0]).addTo(map);
@@ -10,24 +15,29 @@ marker.bindTooltip(
   `<img src="./assets/logo.png" alt="logo" style="width:5rem;" /> <br/> is here!`
 );
 
-if (navigator.geolocation) {
-  navigator.geolocation.watchPosition(
-    (position) => {
-      const userLat = position.coords.latitude;
-      const userLng = position.coords.longitude;
+function findLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+      (position) => {
+        const userLat = position.coords.latitude;
+        const userLng = position.coords.longitude;
 
-      marker.setLatLng([userLat, userLng]);
-      map.setView([userLat, userLng], 15);
-    },
-    (error) => {
-      console.error(`Geolocation error: ${error.message}`);
-    },
-    {
-      enableHighAccuracy: true,
-      maximumAge: 0,
-      timeout: 5000,
-    }
-  );
-} else {
-  console.error("Geolocation is not supported by this browser.");
+        marker.setLatLng([userLat, userLng]);
+        map.setView([userLat, userLng], 15);
+      },
+      (error) => {
+        console.error(`Geolocation error: ${error.message}`);
+      },
+      {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000,
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
 }
+
+//*******call function on page load if supported by the browser*******
+findLocation();
